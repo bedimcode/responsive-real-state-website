4da3a9eb4f1c71aea5422f59c476cb742c5f1658
bedimcode <61248995+bedimcode@users.noreply.github.com>
2022-04-29 09:36:08 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SWIPER POPULAR ===============*/
+
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
